65566a3b434d140c7b80b8d6ff8f225768c922df
papa ahmadou fall <pahmadoudou@gmil.com>
2025-03-19 03:33:34 +0000

    adding relation between burger and command

--- app/app/Models/burger.php
+++ app/app/Models/burger.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class burger extends Model
 {
@@ -17,5 +18,8 @@ class burger extends Model
         'stock',
     ];
 
+    public function commands(): BelongsToMany
+{
+    return $this->belongsToMany(Command::class, 'command')->withPivot('quantite')->withTimestamps();
 }
-return $this->belongsToMany(Command::class, 'command')->withPivot('quantite')->withTimestamps();
+}
